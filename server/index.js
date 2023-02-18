d20e5b7f62af73284afcaee3b4ef933f2f2f7c0a
aluday <anhluu322@gmail.com>
2023-02-18 18:49:48 +0700

    dinh nghia cac route cho tai nguyen contact

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const contactRouter = require('./router/contact.route');
 const app = express();
-app.use(cors());
-app.use(express.json());
 const port = 3000;
 
 
-app.get("/", (req, res)=>{
-    res.json({message: "Welcome to connact book application"});
-})
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/api/contact', contactRouter);
 
 app.listen(port, ()=>{
     console.log(`Server is running on ${port}`);

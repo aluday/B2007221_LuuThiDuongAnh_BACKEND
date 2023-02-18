c52cec5636d1a8e0e30347540a89e73fe336f4d2
aluday <anhluu322@gmail.com>
2023-02-18 19:03:20 +0700

    cai dat xu ly loi

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const contactRouter = require('./router/contact.route');
+const ApiError = require('./helpers/api-error');
 const app = express();
 const port = 3000;
 
@@ -10,6 +11,17 @@ app.use(express.json());
 
 
 app.use('/api/contact', contactRouter);
+
+app.use((req, res, next)=>{
+    return next(new ApiError(404, "Resource not found"));
+})
+
+app.use((error, req, res, next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal server error"
+    })
+})
+
 
 app.listen(port, ()=>{
     console.log(`Server is running on ${port}`);
